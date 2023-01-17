4ea5952ce8ff1ce67c12cc70c09fe7ec2c0765ef
unknown <mjssmith03@gmail.com>
2023-01-17 15:09:44 +0100

    It now shows the next 3 blocks.

--- Assets/Scripts/incomingBlocks.cs
+++ Assets/Scripts/incomingBlocks.cs
@@ -2,13 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class incomingBlocks : MonoBehaviour
 {
+    public List<GameObject> imagePlaces = new List<GameObject>();
+    public List<Sprite> sprites = new List<Sprite>();
+
     private List<GameObject> blocks;
 
-    private List<GameObject> currentNextBlocks;
+    private List<GameObject> currentNextBlocks = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +21,8 @@ public class incomingBlocks : MonoBehaviour
         try
         {
             currentNextBlocks.Add(blocks[Random.Range(0,6)]);
-            // currentNextBlocks.Add(blocks[Random.Range(0,6)]);
-            // currentNextBlocks.Add(blocks[Random.Range(0,6)]);
+            currentNextBlocks.Add(blocks[Random.Range(0,6)]);
+            currentNextBlocks.Add(blocks[Random.Range(0,6)]);
         }
         catch (Exception e)
         {
@@ -30,13 +34,52 @@ public class incomingBlocks : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.Tab))
-        // {
-        //     currentNextBlocks.Add(blocks[Random.Range(0,6)]);
-        //     currentNextBlocks.RemoveAt(0);
-        //     Debug.Log(currentNextBlocks);
-        // }
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            getNextBlocks();
+        }
     }
-    
-    
+
+    private void getNextBlocks()
+    {
+        currentNextBlocks.Add(blocks[Random.Range(0,7)]);
+        currentNextBlocks.RemoveAt(0);
+        for (int i = 0; i < currentNextBlocks.Count; i++)
+        {
+            Debug.Log(i + ": " + currentNextBlocks[i].name);
+        }
+        Debug.Log(currentNextBlocks);
+
+        for (int i = 0; i < imagePlaces.Count; i++)
+        {
+            switch (currentNextBlocks[i].name)
+            {
+                case "IBlock":
+                    imagePlaces[i].GetComponent<Image>().sprite = sprites[0];
+                    break;
+                case "JBlock":
+                    imagePlaces[i].GetComponent<Image>().sprite = sprites[1];
+                    break;
+                case "LBlock":
+                    imagePlaces[i].GetComponent<Image>().sprite = sprites[2];
+                    break;
+                case "OBlock":
+                    imagePlaces[i].GetComponent<Image>().sprite = sprites[3];
+                    break;
+                case "SBlock":
+                    imagePlaces[i].GetComponent<Image>().sprite = sprites[4];
+                    break;
+                case "TBlock":
+                    imagePlaces[i].GetComponent<Image>().sprite = sprites[5];
+                    break;
+                case "ZBlock":
+                    imagePlaces[i].GetComponent<Image>().sprite = sprites[6];
+                    break;
+                default:
+                    imagePlaces[i].GetComponent<Image>().sprite = sprites[7];
+                    break;
+            }
+
+        }
+    }
 }
